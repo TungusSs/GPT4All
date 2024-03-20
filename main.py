420fdfc627cf129fb3bdec9678f54dbbd7298880
oustery <oustery@mail.ru>
2024-03-20 19:22:28 +0300

    Impoved code structure and fixed bugs

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
-import os
-
-import g4f
+import discord
 from discord.ext import commands
-from discord import Intents
-
+from discord import Intents, app_commands
 from decouple import config
 
+from Utils.api import API
+
+
 intents = Intents.default()
-intents.guilds = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -19,39 +18,32 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} был подключён к Discord!")
+    
+    try:
+        synced = await bot.tree.sync()
+        print(f"Синхронизировано {len(synced)} команд.")
+    except Exception as e:
+        print(f"Возникла ошибка: {e}")
+
+    await bot.wait_until_ready()
 
 
-# @bot.event
-# async def on_command_error(ctx, error):
-#     if isinstance(error, commands.errors.CheckFailure):
-#         await ctx.send("При отправке команды вы не указали запрос.")
+@bot.tree.command(name="gpt", description="Сгенерировать ответ на запрос.")
+@app_commands.describe(prompt="Ваш запрос")
+async def gpt(interaction: discord.Interaction, prompt: str):
+    await interaction.response.defer(ephemeral=True, thinking=True)
+    
+    response = await API.get_answer(prompt)
+    
+    await interaction.followup.send(f"*Ответ:*\n{response}")
 
 
-@bot.command(name="gpt")
-async def gpt(ctx, *, args):
-    if args:
-        response = await g4f.ChatCompletion.create_async(
-            model=g4f.models.default,
-            messages=[{"role": "user", "content": args}],
-            provider=g4f.Provider.Bing,
-        )
-
-        await ctx.send(f"{response}")
-        # await ctx.send(f"Привет. Ты использовал команду `/gpt`. Так же ты мне написал текст: `{args}`")
-
-
-# TODO: Реализовать генерацию изображений (пока не реализована).
-@bot.command(name="imagine")
-async def imagine(ctx, *, args):
-    if args:
-        response = await g4f.ChatCompletion.create_async(
-            model=g4f.models.default,
-            messages=[{"role": "user", "content": args}],
-            provider=g4f.Provider.Bing,
-        )
-
-        await ctx.send(f"{response}")
-        # await ctx.send(f"Привет. Ты использовал команду `/imagine`. Так же ты мне написал текст: `{args}`")
+# TODO: Реализовать генерацию изображений (пока не реализована)
+@bot.tree.command(name="imagine", description="Сгенерировать изображение.")
+@app_commands.describe(prompt="Ваш запрос")
+async def imagine(interaction: discord.Interaction, prompt: str):
+    await interaction.response.defer(ephemeral=True, thinking=True)
+    await interaction.followup.send(f"*⚠️ Эта команда временно не работает ⚠️*")
 
 
 bot.run(config("DISCORD_TOKEN"))
