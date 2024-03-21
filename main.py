a854ccb55932b7d1c079852ef831483e3e312567
oustery <oustery@mail.ru>
2024-03-21 18:30:17 +0300

    Updated code structure and added error catching

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
+import os
+
 import discord
 from discord.ext import commands
 from discord import Intents, app_commands
 from decouple import config
 
 from Utils.api import API
+from Utils.discord_tools import DiscordTools
 
 
 intents = Intents.default()
@@ -28,22 +31,35 @@ async def on_ready():
     await bot.wait_until_ready()
 
 
-@bot.tree.command(name="gpt", description="Сгенерировать ответ на запрос.")
+@bot.tree.command(name="ask", description="Сгенерировать ответ на запрос.")
 @app_commands.describe(prompt="Ваш запрос")
-async def gpt(interaction: discord.Interaction, prompt: str):
+async def ask(interaction: discord.Interaction, prompt: str):
     await interaction.response.defer(ephemeral=True, thinking=True)
     
     response = await API.get_answer(prompt)
     
-    await interaction.followup.send(f"*Ответ:*\n{response}")
+    if response.status != 200:
+        await interaction.followup.send(f"**Ваш запрос:** *{prompt}*\n**Результат**:*⚠️ {response.message} ⚠️*")
+    else:
+        await interaction.followup.send(f"**Ответ:**\n{response.message}")
 
 
-# TODO: Реализовать генерацию изображений (пока не реализована)
-@bot.tree.command(name="imagine", description="Сгенерировать изображение.")
+@bot.tree.command(name="imagine", description="Сгенерировать изображения.")
 @app_commands.describe(prompt="Ваш запрос")
 async def imagine(interaction: discord.Interaction, prompt: str):
     await interaction.response.defer(ephemeral=True, thinking=True)
-    await interaction.followup.send(f"*⚠️ Эта команда временно не работает ⚠️*")
+    
+    response = await API.get_image(prompt, interaction.user.id)
+    
+    if response.status != 200:
+        await interaction.followup.send(f"**Ваш запрос:** *{prompt}*\n**Результат**:*⚠️ {response.message} ⚠️*")
+    else:      
+        files = DiscordTools.convert2files(response.files)
+          
+        await interaction.followup.send(f"**Ваш запрос:** *{prompt}*\n**Изображения:**", files=files)
+        
+        for image in response.files:
+            os.remove(image)
 
 
 bot.run(config("DISCORD_TOKEN"))
